776df86c326bbebbd3907c7b9c3c1ab3b2a5edd7
Ferhat EFİL <32821105+ferate@users.noreply.github.com>
2022-11-01 15:39:58 +0300

    After Day 10 , Homework 1
    After Day 10 , Homework 1

---
+++ AfterDay10/Business/Concrete/ProductManager.cs
@@ -0,0 +1,64 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Result;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        // Burada Inject edeceğimiz yapının temel Interfaceini veririz.
+        // Burada ne InMemory ne de EntityFramework geçmemeli
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public IResult Delete(Product product)
+        {
+            _productDal.Delete(product);
+            return new SuccessResult(Messages.ProductDeleted);
+        }
+        
+        public IDataResult<List<Product>> GetAll()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
+        }
+        
+        public IDataResult<Product> GetById(int id)
+        {
+            return new SuccessDataResult<Product>(_productDal.GetById(p => p.ProductId == id),Messages.ProductShowed);
+        }
+
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
+        {
+            if(System.DateTime.Now.Hour==15)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult< List < ProductDetailDto >>(_productDal.GetProductDetails(),Messages.ProductDetailListed);
+        }
+
+        public IResult Update(Product product)
+        {
+            _productDal.Update(product);
+            return new SuccessResult(Messages.ProductUpdated);
+        }
+    }
+}
---
+++ AfterDay10/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,38 @@
+﻿using Core.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+      
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from P in context.Products
+                             join c in context.Categories
+                             on P.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductId = P.ProductId,
+                                 ProductName = P.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = P.UnitsInStock
+                             };
+
+                return result.ToList();
+            }
+        }
+
+    }
+}
