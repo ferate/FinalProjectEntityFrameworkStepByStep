b17eea8688b84d70e8c9e6c2eeb4dcd19cd92aaa
Ferhat EFİL <32821105+ferate@users.noreply.github.com>
2022-11-04 16:13:24 +0300

    After Day 13
    After Day 13

---
+++ AfterDay13/Business/Abstract/IProductService.cs
@@ -0,0 +1,24 @@
+﻿using Core.Utilities.Result;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        //List<Product> GetAll(Expression<Func<Product, bool>> filter = null);
+
+        IDataResult<List<Product>> GetAll();
+        IDataResult<Product> GetById(int id);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IResult Add(Product product);
+        IResult Update(Product product);
+        IResult Delete(Product product);
+    }
+}
