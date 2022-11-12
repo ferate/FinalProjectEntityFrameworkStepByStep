d91532878dd86f9a3f6ed0091301ca95d0e53a54
Ferhat EFİL <32821105+ferate@users.noreply.github.com>
2022-11-12 11:52:55 +0300

    JWT Entegrasyonu Hatalar Giderilerek Eklendi, Adım Adım Anlatıldı
    JWT Entegrasyonu Hatalar Giderilerek Eklendi, Adım Adım Anlatıldı

---
+++ AfterDay14/Business/Concrete/EmployeeManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class EmployeeManager : IEmployeeService
+    {
+        IEmployeeDal _employeeDal;
+
+        public EmployeeManager(IEmployeeDal employeeDal)
+        {
+            _employeeDal = employeeDal;
+        }
+
+        public void Add(Employee employee)
+        {
+            _employeeDal.Add(employee);
+        }
+
+        public void Delete(Employee employee)
+        {
+            _employeeDal.Delete(employee);
+        }
+
+        public List<Employee> GetAll()
+        {
+            return _employeeDal.GetAll();
+        }
+
+        public Employee GetById(int id)
+        {
+            return _employeeDal.GetById(e=>e.EmployeeID==id);
+        }
+
+        public void Update(Employee employee)
+        {
+            _employeeDal.Update(employee);
+        }
+    }
+}
---
+++ AfterDay14/DataAccess/Abstract/NorthwindContext.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+
+    }
+}
